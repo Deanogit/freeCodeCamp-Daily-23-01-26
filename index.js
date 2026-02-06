4bd6497cff28877b257e2ed0270ef2de7aee909e
Dean Howe <hideanhowe@gmail.com>
2026-02-06 17:26:55 +0000

    solution

--- index.js
+++ index.js
@@ -4,3 +4,13 @@
 // Start with a #, and
 // be followed by either 3 or 6 hexadecimal characters.
 // Hexadecimal characters are numbers 0 through 9 and letters a through f (case-insensitive).
+
+function isValidHex(str) {
+  console.log(str);
+  // start with #
+  // const regex = /^#/
+  // followed by either 3 or 6 hexadecimal characters
+  const regex = /^#([a-f0-9]{6}|[a-f0-9]{3})$/i;
+  return regex.test(str);
+  // return str;
+}
